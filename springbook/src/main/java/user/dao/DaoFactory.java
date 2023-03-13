7635a395c4ed14d1e96469e49764d28da9e531dd
Greg.Study <greg0917@naver.com>
2023-03-14 03:36:10 +0900

    스프링 설정

--- springbook/src/main/java/user/dao/DaoFactory.java
+++ springbook/src/main/java/user/dao/DaoFactory.java
@@ -1,9 +1,19 @@
 package user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
 
+    @Bean
     public UserDao userDao() {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        return new UserDao(new DConnectionMaker());
+        return new UserDao(connectionMaker());
     }
+
+    @Bean
+    public ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
+    }
+
 }
--- springbook/src/test/java/user/dao/UserDaoTest.java
+++ springbook/src/test/java/user/dao/UserDaoTest.java
@@ -1,10 +1,14 @@
 package user.dao;
 
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 class UserDaoTest {
 
     public static void main(String[] args) throws ClassNotFoundException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao userDao = context.getBean("userDao", UserDao.class);
     }
 
 }

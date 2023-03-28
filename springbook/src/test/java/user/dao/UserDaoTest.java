063ed1dbdb45f7a9a4808c00dbad3d641247bf69
Greg.Study <greg0917@naver.com>
2023-03-29 03:08:43 +0900

    2장 - 테스트

--- springbook/src/test/java/user/dao/UserDaoTest.java
+++ springbook/src/test/java/user/dao/UserDaoTest.java
@@ -1,14 +1,34 @@
 package user.dao;
 
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import user.domain.User;
 
-class UserDaoTest {
+import java.sql.SQLException;
 
-    public static void main(String[] args) throws ClassNotFoundException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao userDao = context.getBean("userDao", UserDao.class);
+import static org.assertj.core.api.Assertions.*;
+
+public class UserDaoTest {
+
+    @Test
+    public void addAndGet() throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        UserDao userDao = context.getBean("UserDao", UserDao.class);
+
+        User user = new User();
+        user.setId("greg");
+        user.setName("그렉");
+        user.setPassword("spring1");
+
+        userDao.add(user);
+        User user2 = userDao.get(user.getId());
+
+        assertThat(user2.getName()).isEqualTo(user.getName());
+        assertThat(user2.getPassword()).isEqualTo(user.getPassword());
     }
 
 }
